7bb348a771ed5ed3f683fed9265817ff29946a29
Dmitriy Ashnevits <dmitriy.ashnevits@gs-labs.ru>
2023-06-22 01:55:26 +0300

    Some tests

--- tests/SolverTest.cpp
+++ tests/SolverTest.cpp
@@ -1,9 +1,40 @@
-// #include <mylib/mylib.h>
-#include <iostream>
+#include "Solver.h"
 
 #include <gtest/gtest.h>
 
-TEST(add_test, add_1_1)
+class SolverTest : public ::testing::Test
 {
-    std::cout << "Hello, from homework!\n";
+public:
+    SolverTest() = default;
+
+protected:
+    Solver::Solver solver;
+};
+
+TEST_F(SolverTest, no_roots)
+{
+    const auto roots = solver.solve(1, 0, 1);
+    EXPECT_EQ(roots.size(), 0);
+}
+
+TEST_F(SolverTest, two_roots)
+{
+    const auto roots = solver.solve(1, 0, -1);
+    EXPECT_EQ(roots.size(), 2);
+    EXPECT_EQ(roots.at(0), 1);
+    EXPECT_EQ(roots.at(1), -1);
+}
+
+TEST_F(SolverTest, two_roots_of_one)
+{
+    const auto roots = solver.solve(1, 2, 1);
+    EXPECT_EQ(roots.size(), 2);
+    EXPECT_TRUE(roots.at(0) == roots.at(1));
+    EXPECT_EQ(roots.at(0), -1);
+    EXPECT_EQ(roots.at(1), -1);
+}
+
+TEST_F(SolverTest, throw_on_zero_a)
+{
+    EXPECT_THROW(solver.solve(0, 1, 1), std::exception);
 }
