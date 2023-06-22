7ce62e3aac82e220c15badae4cb790dcf91f6638
Dmitriy Ashnevits <dmitriy.ashnevits@gs-labs.ru>
2023-06-22 23:46:08 +0300

    Implementation done

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -1,10 +1,44 @@
 #include "Solver.h"
+#include <cmath>
+#include <stdexcept>
 
 namespace Solver
 {
     std::vector<double> Solver::solve(double a, double b, double c) const
     {
-        return std::vector<double> {};
+        std::vector<double> result{};
+        if (std::abs(a) <= std::numeric_limits<double>::epsilon())
+        {
+            throw std::invalid_argument("a is zero");
+        }
+        else if (std::isnan(a) || std::isnan(b) || std::isnan(c))
+        {
+            throw std::invalid_argument("args is nan");
+        }
+        else if (std::isinf(a) || std::isinf(b) || std::isinf(c))
+        {
+            throw std::invalid_argument("args is infinity");
+        }
+        else
+        {
+            double D = b * b - 4 * a * c;
+            if (!(D < -std::numeric_limits<double>::epsilon()))
+            {
+                if (std::abs(D) <= std::numeric_limits<double>::epsilon())
+                {
+                    const auto root = -b / (2 * a);
+                    result = {root, root};
+                }
+                else if (D > std::numeric_limits<double>::epsilon())
+                {
+                    result = {
+                        (-b + std::sqrt(D)) / (2 * a),
+                        (-b - std::sqrt(D)) / (2 * a)
+                    };
+                }
+            }
+        }
+        return result;
     }
 
 } // namespace Solver
--- tests/SolverTest.cpp
+++ tests/SolverTest.cpp
@@ -1,4 +1,5 @@
 #include "Solver.h"
+#include <cmath>
 
 #include <gtest/gtest.h>
 
@@ -8,6 +9,11 @@ public:
     SolverTest() = default;
 
 protected:
+    bool compare(double a, double b) const
+    {
+        return std::abs(a - b) < std::numeric_limits<double>::epsilon();
+    }
+
     Solver::Solver solver;
 };
 
@@ -21,20 +27,42 @@ TEST_F(SolverTest, two_roots)
 {
     const auto roots = solver.solve(1, 0, -1);
     EXPECT_EQ(roots.size(), 2);
-    EXPECT_EQ(roots.at(0), 1);
-    EXPECT_EQ(roots.at(1), -1);
+    EXPECT_TRUE(compare(roots.at(0), 1));
+    EXPECT_TRUE(compare(roots.at(1), -1));
 }
 
 TEST_F(SolverTest, two_roots_of_one)
 {
     const auto roots = solver.solve(1, 2, 1);
     EXPECT_EQ(roots.size(), 2);
-    EXPECT_TRUE(roots.at(0) == roots.at(1));
-    EXPECT_EQ(roots.at(0), -1);
-    EXPECT_EQ(roots.at(1), -1);
+    EXPECT_TRUE(compare(roots.at(0), roots.at(1)));
+    EXPECT_TRUE(compare(roots.at(0), -1));
+    EXPECT_TRUE(compare(roots.at(1), -1));
 }
 
 TEST_F(SolverTest, throw_on_zero_a)
 {
-    EXPECT_THROW(solver.solve(0, 1, 1), std::exception);
+    EXPECT_THROW(solver.solve(0, 1, 1), std::invalid_argument);
+}
+
+TEST_F(SolverTest, small_coefficents)
+{
+    const auto roots = solver.solve(0.00000000000000127, 0.0000000000000002347, 0.0000000000000005667);
+    EXPECT_EQ(roots.size(), 2);
+    EXPECT_TRUE(compare(roots.at(0), roots.at(1)));
+}
+
+TEST_F(SolverTest, throw_on_NAN)
+{
+    EXPECT_THROW(solver.solve(NAN, NAN, NAN), std::invalid_argument);
+}
+
+TEST_F(SolverTest, throw_on_std_nan)
+{
+    EXPECT_THROW(solver.solve(std::nan("1"), std::nan("2"), std::nan("3")), std::invalid_argument);
+}
+
+TEST_F(SolverTest, throw_on_INFINITY)
+{
+    EXPECT_THROW(solver.solve(INFINITY, -INFINITY, INFINITY), std::invalid_argument);
 }
